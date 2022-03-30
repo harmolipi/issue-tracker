bacb479c0aef56778a7313f123ff63bad682e3f2
harmolipi <nikob381@gmail.com>
2022-03-30 13:02:14 -0400

    Created issue controller skeleton

---
+++ controllers/issueController.js
@@ -0,0 +1,17 @@
+const Issue = require('../models/issue');
+
+exports.issue_create_post = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Issue create POST');
+};
+
+exports.issue_view_get = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Issue view GET');
+};
+
+exports.issue_update_put = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Issue update PUT');
+};
+
+exports.issue_delete = (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Issue delete');
+};
