097294c6384c617a0e928df5b7c3da7b1769740c
harmolipi <nikob381@gmail.com>
2022-03-30 15:42:40 -0400

    Import project controller

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,25 @@
 'use strict';
 
-module.exports = function (app) {
+const issue_controller = require('../controllers/issueController');
+const project_controller = require('../controllers/projectController');
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+module.exports = function (app) {
+  app
+    .route('/api/issues/:project')
+
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
