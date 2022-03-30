30684bbc053fb66025933ae5de492b41eed2348e
harmolipi <nikob381@gmail.com>
2022-03-30 13:31:14 -0400

    Created project model

---
+++ models/project.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema({
+  project: {
+    type: String,
+    required: true,
+  },
+  issues: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Issue',
+    },
+  ],
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
