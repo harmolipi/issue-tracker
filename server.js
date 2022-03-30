21ddae20450f1785ffeb77d05425f067e6e6528d
harmolipi <nikob381@gmail.com>
2022-03-30 13:04:41 -0400

    Fixed MONGO_URI variable name

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner');
 
 const mongoose = require('mongoose');
-const mongoDB = process.env.MONGODB_URI;
+const mongoDB = process.env.MONGO_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
