3f6f7c48824507403ba7fdc648aba3721e81a5d2
harmolipi <nikob381@gmail.com>
2022-04-19 11:27:26 -0400

    Create issue

--- controllers/issueController.js
+++ controllers/issueController.js
@@ -1,17 +1,38 @@
 const Issue = require('../models/issue');
 
-exports.issue_create_post = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Issue create POST');
+exports.issue_create_post = (req, res, project, next) => {
+    const issue = new Issue({
+        _id: req.body._id,
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to,
+        status_text: req.body.status_text,
+        project: project,
+    });
+
+    issue.save((err, issue) => {
+        if (err) {
+            const error =
+                err.errors.issue_title.kind === 'required' ||
+                err.errors.issue_text.kind === 'required' ||
+                err.errors.created_by.kind === 'required' ?
+                'required field(s) missing' :
+                err;
+            res.status(500).json({ error });
+        }
+        res.status(201).json(issue);
+    });
 };
 
 exports.issue_view_get = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Issue view GET');
+    res.send('NOT IMPLEMENTED: Issue view GET');
 };
 
 exports.issue_update_put = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Issue update PUT');
+    res.send('NOT IMPLEMENTED: Issue update PUT');
 };
 
 exports.issue_delete = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Issue delete');
+    res.send('NOT IMPLEMENTED: Issue delete');
 };
--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const issue_controller = require('../controllers/issueController');
-const project_controller = require('../controllers/projectController');
 
-module.exports = function (app) {
-  app
-    .route('/api/issues/:project')
+module.exports = function(app) {
+    app
+        .route('/api/issues/:project')
 
-    .get(function (req, res) {
-      let project = req.params.project;
+    .get(function(req, res) {
+        const project = req.params.project;
     })
 
-    .post(function (req, res) {
-      let project = req.params.project;
+    .post(function(req, res) {
+        const project = req.params.project;
+        issue_controller.issue_create_post(req, res, project);
     })
 
-    .put(function (req, res) {
-      let project = req.params.project;
+    .put(function(req, res) {
+        const project = req.params.project;
     })
 
-    .delete(function (req, res) {
-      let project = req.params.project;
+    .delete(function(req, res) {
+        const project = req.params.project;
     });
 };
