5d92b47c03a0b6fba0aa79974e3476b1882a35a8
harmolipi <nikob381@gmail.com>
2022-04-19 11:26:53 -0400

    Unique project model name

--- models/project.js
+++ models/project.js
@@ -2,16 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProjectSchema = new Schema({
-  project: {
-    type: String,
-    required: true,
-  },
-  issues: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Issue',
+    project: {
+        type: String,
+        required: true,
+        unique: true,
     },
-  ],
+    issues: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Issue',
+    }, ],
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
