067e10bc3c4fa259d25c5cf7fffdd78735a52a66
harmolipi <nikob381@gmail.com>
2022-04-19 11:26:31 -0400

    Added project to issue model

--- models/issue.js
+++ models/issue.js
@@ -2,34 +2,38 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const IssueSchema = new Schema({
-  issue_title: {
-    type: String,
-    required: true,
-  },
-  issue_text: {
-    type: String,
-    required: true,
-  },
-  created_by: {
-    type: String,
-    required: true,
-  },
-  assigned_to: {
-    type: String,
-    default: '',
-  },
-  status_text: {
-    type: String,
-    default: '',
-  },
-  created_on: {
-    type: Date,
-    default: Date.now,
-  },
-  updated_on: {
-    type: Date,
-    default: Date.now,
-  },
+    issue_title: {
+        type: String,
+        required: true,
+    },
+    issue_text: {
+        type: String,
+        required: true,
+    },
+    created_by: {
+        type: String,
+        required: true,
+    },
+    assigned_to: {
+        type: String,
+        default: '',
+    },
+    status_text: {
+        type: String,
+        default: '',
+    },
+    created_on: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_on: {
+        type: Date,
+        default: Date.now,
+    },
+    project: {
+        type: String,
+        required: true,
+    },
 });
 
 module.exports = mongoose.model('Issue', IssueSchema);
