f432f7fd0b0c1aba18f01b591190b3067f7adc66
harmolipi <nikob381@gmail.com>
2022-04-20 09:59:48 -0400

    Removed unnecessary import

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expect = require('chai').expect;
 const cors = require('cors');
 require('dotenv').config();
 
@@ -26,13 +25,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Sample front-end
-app.route('/:project/').get(function (req, res) {
-  res.sendFile(process.cwd() + '/views/issue.html');
+app.route('/:project/').get(function(req, res) {
+    res.sendFile(process.cwd() + '/views/issue.html');
 });
 
 //Index page (static HTML)
-app.route('/').get(function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.route('/').get(function(req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 //For FCC testing purposes
@@ -42,24 +41,24 @@ fccTestingRoutes(app);
 apiRoutes(app);
 
 //404 Not Found Middleware
-app.use(function (req, res, next) {
-  res.status(404).type('text').send('Not Found');
+app.use(function(req, res, next) {
+    res.status(404).type('text').send('Not Found');
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if (process.env.NODE_ENV === 'test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch (e) {
-        console.log('Tests are not valid:');
-        console.error(e);
-      }
-    }, 3500);
-  }
+const listener = app.listen(process.env.PORT || 3000, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+    if (process.env.NODE_ENV === 'test') {
+        console.log('Running Tests...');
+        setTimeout(function() {
+            try {
+                runner.run();
+            } catch (e) {
+                console.log('Tests are not valid:');
+                console.error(e);
+            }
+        }, 3500);
+    }
 });
 
 module.exports = app; //for testing
